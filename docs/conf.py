1e9d46833fc9516cdb4d11d774e75737fcd687c2
MichaelHopwood <michael.hopwood@knights.ucf.edu>
2021-08-24 20:19:09 -0400

    add CV

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "nbsphinx", "nbsphinx_link", "sphinxcontrib.pdfembed"]
+extensions = ["sphinx.ext.autodoc", "nbsphinx", "nbsphinx_link"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
